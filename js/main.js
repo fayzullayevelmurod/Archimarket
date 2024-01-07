2cd218366a78a536f2400cdbb569ce3e9d3a37fe
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-01-08 00:49:29 +0500

    youtube channel section add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('Salom');
+
+// youtube channel
+let swiper_one = new Swiper(".youtubeSwiper", {
+    navigation: {
+      nextEl: ".channel_next",
+      prevEl: ".channel_prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        993: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+    },
+});
+// youtube channel
